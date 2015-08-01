9c663546d15c0928427764ccf39d8f49710ff895
Yuang <yuang_liao@163.com>
2015-08-01 14:53:48 +0800

    新增数据库帮助类，以及各表对应的实体类

---
+++ app/src/main/java/com/example/shyuan/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.example.shyuan.coolweather.model;
+
+/**
+ * Created by shyuan on 2015/7/31.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String ProvinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return ProvinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        ProvinceCode = provinceCode;
+    }
+}
