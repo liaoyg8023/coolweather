c6692dab0c9025b8cf62f92eed29fa39aa73ead5
Yuang <yuang_liao@163.com>
2015-08-02 17:30:47 +0800

    设置后台自动更新以及更换app图标等

--- app/src/main/java/com/example/shyuan/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/shyuan/coolweather/activity/WeatherActivity.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.shyuan.coolweather.R;
+import com.example.shyuan.coolweather.service.autoUpdateService;
 import com.example.shyuan.coolweather.util.HttpCallbackListener;
 import com.example.shyuan.coolweather.util.HttpUtil;
 import com.example.shyuan.coolweather.util.Utility;
@@ -81,6 +82,9 @@ public class WeatherActivity extends Activity implements View.OnClickListener{
         currentDateText.setText(prefs.getString("current_date", ""));
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, autoUpdateService.class);
+        startService(intent);
     }
 
     private void queryWeatherCode(String countyCode) {
